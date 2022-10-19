7832f59b808c556f9237a55c46e29757e7f13af1
Riddick <riddick.angiela@student.moringaschool.com>
2022-10-19 07:46:56 +0300

    Completed assignment

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -1,54 +1,33 @@
-require_relative 'spec_helper'
-require_relative '../methods'
-
-describe '#greet_programmer' do
-
-  it 'outputs the string "Hello, programmer!"' do
-    expect { greet_programmer }.to output(a_string_including("Hello, programmer!")).to_stdout
-  end
-  
+# Define a method #greet_programmer that takes no arguments. It should output the string "Hello, programmer!" to the terminal with #puts
+def greet_programmer
+    puts "Hello, programmer!"
 end
 
-describe '#greet' do
+greet_programmer
 
-  it 'outputs a string "Hello, Naureen!" when called with "Naureen"' do
-    expect { greet("Naureen") }.to output(a_string_including("Hello, Naureen!")).to_stdout
-  end
-
-  it 'outputs a string "Hello, Jimmy!" when called with "Jimmy"' do
-    expect { greet("Jimmy") }.to output(a_string_including("Hello, Jimmy!")).to_stdout
-  end
-
+# Define a method #greet that takes one argument, a name. It should output the string "Hello, name!" (with "name" being whatever value was passed as an argument) to the terminal with #puts
+def greet(name)
+    puts "Hello, #{name}!"
 end
 
-describe '#greet_with_default' do
+greet("Ross")
 
-  it 'outputs a string "Hello, Naureen!" when called with "Naureen"' do
-    expect { greet_with_default("Naureen") }.to output(a_string_including("Hello, Naureen!")).to_stdout
-  end
-
-  it 'outputs a string "Hello, programmer!" when called with no arguments' do
-    expect { greet_with_default }.to output(a_string_including("Hello, programmer!")).to_stdout
-  end
-
+# Define a method #greet_with_default that takes one argument, a name. It should output the string "Hello, name!" (with "name" being whatever value was passed as an argument) to the terminal with #puts. If no arguments are passed in, it should output the string "Hello, programmer!"
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
 end
 
-describe '#add' do
-
-  it 'returns the sum of two numbers' do
-    expect(add(2, 5)).to be(7)
-  end
-
+# Define a method #add that takes two numbers as arguments and returns the sum of those two numbers
+def add(num1, num2)
+    return num1 + num2
 end
 
-describe '#halve' do
-  
-  it 'returns half of the given integer' do
-    expect(halve(6)).to be(3)
-  end
-
-  it 'returns nil if not given a integer' do
-    expect(halve("six")).to be(nil)
-  end
-
+# Define a method #halve that takes one number as an argument and returns the that number's value, divided by two. If the argument is not an integer, it should return nil and not throw an error
+def halve(num)
+    if (num).is_a?(Integer)
+        num/2
+    else
+        nil
+    end
 end
+
